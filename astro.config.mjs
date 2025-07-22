96338b230b3210495e45941090f488d484353b54
Serthel <spalaciossergioandres@gmail.com>
2025-07-22 12:56:18 -0500

    Actualizacion cambio de frame

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,6 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-import skeleton from '@skeletonlabs/skeleton/vite';
+import skeleton from '@skeletonlabs/skeleton/astro';
 import react from '@astrojs/react';
 
 // https://astro.build/config
