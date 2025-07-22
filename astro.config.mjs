f259c90b4119c0853ae320c86f04fac966586755
Serthel <spalaciossergioandres@gmail.com>
2025-07-22 13:10:32 -0500

    Actualizacion cambio de frame

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,18 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-import skeleton from '@skeletonlabs/skeleton/astro';
+import tailwindcss from "@tailwindcss/vite";
 import react from '@astrojs/react';
 
 // https://astro.build/config
 export default defineConfig({
   base: '/Sergio_Palacios/',
+  
+  vite: {
+    plugins: [tailwindcss()],
+  },
+  
   integrations: [
     react(),
-    skeleton(), 
+
   ],
 });
