13ebaa3eca5598dc09fa00fd12c0e9c040df9da5
Serthel <spalaciossergioandres@gmail.com>
2025-07-22 18:44:23 -0500

    Actualizacion cambio de frame

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,10 +2,10 @@
 export default {
   content: [
     "./src/**/*.{html,js,ts,jsx,tsx,astro}",
-    "./node_modules/@skeletonlabs/skeleton/**/*.js"
+
   ],
   theme: {
     extend: {},
   },
-  plugins: [require('@skeletonlabs/tw-plugin')],
+
 }
