a1319aa6e57d755544548868df19038685fa8cfa
Serthel <spalaciossergioandres@gmail.com>
2025-07-22 19:53:44 -0500

    Updates

--- _astro/Header.AXpMaiSN.js
+++
@@ -1,9 +0,0 @@
-import"./index.9MVAkNgL.js";var u={exports:{}},e={};/**
- * @license React
- * react-jsx-runtime.production.js
- *
- * Copyright (c) Meta Platforms, Inc. and affiliates.
- *
- * This source code is licensed under the MIT license found in the
- * LICENSE file in the root directory of this source tree.
- */var s;function R(){if(s)return e;s=1;var a=Symbol.for("react.transitional.element"),d=Symbol.for("react.fragment");function o(l,r,t){var n=null;if(t!==void 0&&(n=""+t),r.key!==void 0&&(n=""+r.key),"key"in r){t={};for(var i in r)i!=="key"&&(t[i]=r[i])}else t=r;return r=t.ref,{$$typeof:a,type:l,key:n,ref:r!==void 0?r:null,props:t}}return e.Fragment=d,e.jsx=o,e.jsxs=o,e}var x;function p(){return x||(x=1,u.exports=R()),u.exports}var v=p();function E(){return v.jsx("header",{className:"bg-indigo-700 text-white p-4 text-center text-2xl font-bold",children:"Mi Portafolio"})}export{E as default};
