de9d1e2d65e72f42c886dba48f6f97abbc92f061
Serthel <spalaciossergioandres@gmail.com>
2025-07-22 19:39:10 -0500

    Actualizacion cambio de frame

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,23 @@
 /** @type {import('tailwindcss').Config} */
-export default {
+module.exports = {
   content: [
-    "./src/**/*.{html,js,ts,jsx,tsx,astro}",
-
+    "./src/**/*.{astro,html,js,jsx,ts,tsx,vue,svelte}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: '#6D28D9',
+        secondary: '#9333EA',
+        background: '#F3F4F6',
+        dark: '#111827',
+        accent: '#F59E0B',
+      },
+      fontFamily: {
+        sans: ['Inter', 'sans-serif'],
+        heading: ['Poppins', 'sans-serif'],
+      },
+    },
   },
-
+  plugins: [],
 }
+
